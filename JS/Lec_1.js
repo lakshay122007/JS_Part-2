da5d2828b315c43755e293998db3382838295978
lakshay122007 <lakshay11goyal@gmail.com>
2026-01-14 10:08:18 +0530

    Functions

---
+++ JS/Lec_1.js
@@ -0,0 +1,22 @@
+//Arrow function - Kind of Function expression
+
+ //1. Normal way
+
+function hello() {
+    console.log(hello + "HUEHUE")
+}
+
+//2. Expression way
+const hello1 = function() {
+    console.log(hello + "HUEHUE")
+}
+
+//3. Arrow function
+const hello2 = () => console.log(hello + "HUEHUE")
+
+//in Arrow function- it doesn't have name.
+// there is no function keyword, if one parameter Present.
+// no need of brackets.
+// If single line - we can skip curly({}) brackets.
+
+
