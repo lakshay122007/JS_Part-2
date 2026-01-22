5ce9db88335cc348d884bc4a54517e2a2fde571c
lakshay122007 <lakshay11goyal@gmail.com>
2026-01-22 11:14:41 +0530

    JSON Object Parsing

--- JS/Lec_1.js
+++ JS/Lec_1.js
@@ -15,8 +15,21 @@ const hello1 = function() {
 const hello2 = () => console.log(hello + "HUEHUE")
 
 //in Arrow function- it doesn't have name.
+
 // there is no function keyword, if one parameter Present.
 // no need of brackets.
 // If single line - we can skip curly({}) brackets.
 
 
+//JSON PARSING OBJECT
+
+//Fixed Format, Double Quotes Only.
+let backend = `{
+    "name" : "lakshay", 
+    "age" : 123
+}`
+
+let data = JSON.parse(backend)
+console.log(data)
+console.log(data.name)
+console.log(data.age)
