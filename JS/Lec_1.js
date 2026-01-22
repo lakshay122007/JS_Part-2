e351e7b9e7cfe2793386a53e4568d6ed959b2cb3
lakshay122007 <lakshay11goyal@gmail.com>
2026-01-22 12:51:11 +0530

    Question

--- JS/Lec_1.js
+++ JS/Lec_1.js
@@ -1,35 +1,49 @@
-//Arrow function - Kind of Function expression
+// //Arrow function - Kind of Function expression
 
- //1. Normal way
+//  //1. Normal way
 
-function hello() {
-    console.log(hello + "HUEHUE")
-}
+// function hello() {
+//     console.log(hello + "HUEHUE")
+// }
 
-//2. Expression way
-const hello1 = function() {
-    console.log(hello + "HUEHUE")
-}
+// //2. Expression way
+// const hello1 = function() {
+//     console.log(hello + "HUEHUE")
+// }
 
-//3. Arrow function
-const hello2 = () => console.log(hello + "HUEHUE")
+// //3. Arrow function
+// const hello2 = () => console.log(hello + "HUEHUE")
 
-//in Arrow function- it doesn't have name.
+// //in Arrow function- it doesn't have name.
 
-// there is no function keyword, if one parameter Present.
-// no need of brackets.
-// If single line - we can skip curly({}) brackets.
+// // there is no function keyword, if one parameter Present.
+// // no need of brackets.
+// // If single line - we can skip curly({}) brackets.
 
 
-//JSON PARSING OBJECT
+// //JSON PARSING OBJECT
 
-//Fixed Format, Double Quotes Only.
-let backend = `{
-    "name" : "lakshay", 
-    "age" : 123
-}`
+// //Fixed Format, Double Quotes Only.
+// let backend = `{
+//     "name" : "lakshay", 
+//     "age" : 123
+// }`
 
-let data = JSON.parse(backend)
-console.log(data)
-console.log(data.name)
-console.log(data.age)
+// let data = JSON.parse(backend)
+// console.log(data)
+// console.log(data.name)
+// console.log(data.age)
+
+
+
+
+const programLangs = ["Javascript", "C++", "Python", ["Django", "AI/ML"], "Java", {responsiveness: "Media Query"}];
+
+
+const newProgramLangs = [...programLangs];
+
+newProgramLangs[4] = "NodeJS";
+newProgramLangs[3][1] = "ReactJS";
+
+
+console.log(programLangs);
