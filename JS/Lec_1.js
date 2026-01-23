74b252222ad07886dc8b46c6776b148511adbe23
lakshay122007 <lakshay11goyal@gmail.com>
2026-01-23 23:08:27 +0530

    JSON Obj

--- JS/Lec_1.js
+++ JS/Lec_1.js
@@ -47,3 +47,22 @@ newProgramLangs[3][1] = "ReactJS";
 
 
 console.log(programLangs);
+
+
+// The JSON format is syntactically identical to the code for creating JavaScript objects.
+
+// Because of this, a JavaScript program can easily convert JSON data into native JavaScript objects.
+
+// JavaScript has a built in function for converting JSON strings into JavaScript objects:
+
+// JSON.parse()
+
+// JavaScript also has a built in function for converting an object into a JSON string:
+
+// JSON.stringify()
+
+// It is a common mistake to call a JSON object literal "a JSON object".
+
+// JSON cannot be an object. JSON is a string format.
+
+// The data is only JSON when it is in a string format. When it is converted to a JavaScript variable, it becomes a JavaScript object.
