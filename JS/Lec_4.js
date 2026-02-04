1d0f973b00bba4debfee2a4207fac723a7d8d976
lakshay122007 <lakshay11goyal@gmail.com>
2026-02-04 09:29:02 +0530

    function callback

--- JS/Lec_4.js
+++ JS/Lec_4.js
@@ -4,21 +4,36 @@
 
 //describes how are variables defined such that they are accessible for operations or not
 //if variable created inside function --> cannot be accessed outside, thus is called function scope.(var is func scope)
-//if a variable with (let) is created withib a block of code, it cannot ve accessed outside the block.(let/const is blocked scope)
+//if a variable with (let) is created withib a block of code, it cannot be accessed outside the block.(let/const is blocked scope)
 
 var b = 10        //var is function scope
 function print(){
     if(true){
         var a = 60
-        let c = 50 //"let" is blocked scope(cannot be accessed outside a code block)
+        // let c = 50 //"let" is blocked scope(cannot be accessed outside a code block)
         console.log(a) //prints 60
-        console.log(c) //throws error
+        // console.log(c) 
     }
     console.log(a) //prints 60
-    console.log(c) //prints 50
+    // console.log(c) //throws error
 }
 console.log(b)
 print()
 
+
+// <----------------------------------------------------------------------------------->
+
+
+function times(word,num2){
+    console.log(`i want to print ${word} ${num2} times`)
+    print1(word,num2)
+}
+
+function print1(word,num2){
+    for (let i = 0; i < num2; i++){
+        console.log(word)
+    }
+}
+times("hello", 4)
 
 
