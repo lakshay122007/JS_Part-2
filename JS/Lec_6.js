d5eec928f812deade318f555604d96cd87178aac
lakshay122007 <lakshay11goyal@gmail.com>
2026-02-11 09:59:53 +0530

    js eent loop

--- JS/Lec_6.js
+++ JS/Lec_6.js
@@ -3,17 +3,37 @@
 //1) setTimeout() - if we want to execute a particular function after some time - basically delay the execution\
 //syntax - setTimeout(function(){},delay)
 
-console.log(23) //executes first
+// console.log(23) 
 
-setTimeout(function(){
-    console.log("JSCWIC") //executes at last after 2 seconds
-}, 3000)
+// setTimeout(function(){
+//     console.log("JSCWIC") 
+// }, 3000)
 
-setTimeout(function(){
-    console.log("huhuhuh") 
-}, 6000)
+// console.log(329)
 
-console.log(8)//executes second
-console.log("lakshay")
-console.log("goyal")
+// setTimeout(function(){
+//     console.log("huhuhuh")    //TODO: js event loop 
+//     console.log(889)
+// }, 0)
+
+// console.log(8)/
+// console.log("lakshay")
+// console.log("goyal")
+
+
+
+console.log("A")
+
+setTimeout(() => {
+    console.log("B")
+
+    setTimeout(() => {
+        console.log("C")
+    },0)
+    console.log("D")
+},0)
+setTimeout(()=>{
+    console.log("E")
+},0)
+console.log("F")
 
