f010ccf0ca23bddc5d821e3aa2526f58d26f77d1
lakshay122007 <lakshay11goyal@gmail.com>
2026-02-11 09:30:57 +0530

    setTimeout()

--- JS/Lec_6.js
+++ JS/Lec_6.js
@@ -1,2 +1,19 @@
 //TODO: Asynchronus function, setTimeout(), setInterval() 
 
+//1) setTimeout() - if we want to execute a particular function after some time - basically delay the execution\
+//syntax - setTimeout(function(){},delay)
+
+console.log(23) //executes first
+
+setTimeout(function(){
+    console.log("JSCWIC") //executes at last after 2 seconds
+}, 3000)
+
+setTimeout(function(){
+    console.log("huhuhuh") 
+}, 6000)
+
+console.log(8)//executes second
+console.log("lakshay")
+console.log("goyal")
+
