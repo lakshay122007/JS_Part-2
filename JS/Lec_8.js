42ec4e920b42c4b47a18e423009f36575e48a968
lakshay122007 <lakshay11goyal@gmail.com>
2026-02-18 10:12:14 +0530

    Fetch()

---
+++ JS/Lec_8.js
@@ -0,0 +1,16 @@
+//TODO: APIs 
+
+// https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+// https://dummyjson.com/products/1
+
+const url = "https://dummyjson.com/products/1"
+
+//TODO: fetch returns the promise 
+fetch(url)
+.then((val) => val.json()) //this will return a promise
+.then((data) => console.log(data)) //so this 'then' will again check the promise returned above then console the object.
+.catch((err) => console.log(err)) //fetch will fail if wrong API is given
+
+
+
+
