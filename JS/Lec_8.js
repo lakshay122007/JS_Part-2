24a1bd2945bfc5d8eeb3102b08d0732643b00a67
lakshay122007 <lakshay11goyal@gmail.com>
2026-02-23 10:26:39 +0530

    Async and await

--- JS/Lec_8.js
+++ JS/Lec_8.js
@@ -2,10 +2,12 @@
 
 // https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
 // https://dummyjson.com/products/1
+const api = "aaOoCHxkUbibcR5WFBRHaeU4vxz0q0PUL2npsSXN"
 
-const url = "https://dummyjson.com/products/1"
+const url = `https://api.nasa.gov/planetary/apod?api_key=${api}`
 
 //TODO: fetch returns the promise 
+
 fetch(url)
 .then((val) => val.json()) //this will return a promise
 .then((data) => console.log(data)) //so this 'then' will again check the promise returned above then console the object.
---
+++ JS/Lec_9.js
@@ -0,0 +1,33 @@
+// //TODO: ASYNC AND AWAIT 
+
+// console.log(1);
+
+// async function check(){
+//     // setTimeout(() =>{
+//         const p1 = new Promise((resolve,reject) => {
+//             resolve("huehuehue")
+//         })
+//         return p1
+//     // },2000)
+// }
+
+// async function exec(){  
+//     console.log("waiting.......")
+//     const res = await check()
+//     console.log(res)
+//     console.log("done");
+    
+    
+// // this async automatically returns a promise. SO whenever you make any function "async" it will return a promise.
+// }
+// exec() 
+
+const api = "aaOoCHxkUbibcR5WFBRHaeU4vxz0q0PUL2npsSXN"
+const url = `https://api.nasa.gov/planetary/apod?api_key=${api}&date=`
+
+async function getnasa(){
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data);
+}
+getnasa()
